d981afeed71786a49dd5ddde132cccc911395c89
Zhiqiang Du (Inspur Worldwide Services Ltd) <v-zhidu@microsoft.com>
2016-05-10 13:37:18 +0800

    add customlize setting

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,32 @@
+;; init.el --- Emacs configuration
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '())
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+(global-linum-mode t) ;; enable line numbers globally
+
+(require 'window-numbering)
+(window-numbering-mode 1)
+
+(winner-mode 1)
+;; copied from http://puntoblogspot.blogspot.com/2011/05/undo-layouts-in-emacs.html
+(global-set-key (kbd "C-x 4 u") 'winner-undo)
+(global-set-key (kbd "C-x 4 r") 'winner-redo)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'hickey t)
+(provide 'init-preload-local)
